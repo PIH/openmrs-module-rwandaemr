659b08c142907d32452cd241eaccfea8e500d7d2
Cosmin Ioan <cioan@pih.org>
2020-04-16 18:39:53 -0400

    RWA-780, bypass discontinued order date issue

--- migration/rwa-780-quick-update.sql
+++ migration/rwa-780-quick-update.sql
@@ -15,4 +15,10 @@ ADD COLUMN `parent` INT(11) DEFAULT NULL;
 update orders set orderer=8; -- admin account
 UPDATE `orders` SET `discontinued_date`='2017-07-26 00:00:00' WHERE `order_id`='247333';
 UPDATE `orders` SET `discontinued_date`='2020-02-19 00:00:00' WHERE `order_id`='484810';
+update orders set discontinued_by=8 where discontinued = true and discontinued_by is null;
+UPDATE `order_type` SET `java_class_name`='org.openmrs.TestOrder' WHERE `order_type_id`='3';
+UPDATE `order_type` SET `java_class_name`='org.openmrs.TestOrder' WHERE `order_type_id`='4';
+UPDATE `order_type` SET `java_class_name`='org.openmrs.DrugOrder' WHERE `order_type_id`='2';
+UPDATE `order_type` SET `java_class_name`='org.openmrs.DrugOrder' WHERE `order_type_id`='5';
+UPDATE `order_type` SET `java_class_name`='org.openmrs.DrugOrder' WHERE `order_type_id`='1';
 
