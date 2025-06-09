2c0c6dd62223f46ac26bef89f3e29cc87100029e
Michael Seaton <mseaton@pih.org>
2025-06-09 15:54:42 -0400

    RWA-1025 - Add ability to update patient demographics in PACS (#18)

---
+++ api/src/main/java/org/openmrs/module/rwandaemr/radiology/BaseHL7Translator.java
@@ -0,0 +1,116 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.rwandaemr.radiology;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.v23.segment.MSH;
+import ca.uhn.hl7v2.model.v23.segment.PID;
+import ca.uhn.hl7v2.parser.Parser;
+import ca.uhn.hl7v2.parser.PipeParser;
+import org.apache.commons.lang.StringUtils;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.api.ConceptService;
+import org.openmrs.module.emrapi.adt.AdtService;
+import org.openmrs.module.rwandaemr.RwandaEmrConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.UUID;
+
+public abstract class BaseHL7Translator {
+
+    Parser parser = new PipeParser();
+
+    final AdtService adtService;
+    final ConceptService conceptService;
+    final RwandaEmrConfig rwandaEmrConfig;
+
+    public BaseHL7Translator(
+            @Autowired AdtService adtService,
+            @Autowired ConceptService conceptService,
+            @Autowired RwandaEmrConfig rwandaEmrConfig) {
+        this.adtService = adtService;
+        this.conceptService = conceptService;
+        this.rwandaEmrConfig = rwandaEmrConfig;
+    }
+
+    String formatDate(Date date) {
+        return new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH).format(date);
+    }
+
+    String formatDatetime(Date date) {
+        return new SimpleDateFormat("yyyyMMddHHmmss", Locale.ENGLISH).format(date);
+    }
+
+    public static String trim(String value, int maxLength) {
+        return (value == null ? null : StringUtils.substring(value.trim(), 0, maxLength));
+    }
+
+    void populateMshSegment(MSH msh, String sendingFacility, Date messageDate, String messageType, String triggerEvent) throws HL7Exception {
+        msh.getFieldSeparator().setValue("|");
+        msh.getEncodingCharacters().setValue("^~\\&");
+        msh.getSendingApplication().getNamespaceID().setValue("OpenMRS");
+        msh.getSendingFacility().getNamespaceID().setValue(sendingFacility);
+        // Hard-coding PACS_APP and PACS_FACILITY per instruction from vendor
+        msh.getReceivingApplication().getNamespaceID().setValue("PACS_APP");
+        msh.getReceivingFacility().getNamespaceID().setValue("PACS_FACILITY");
+        msh.getDateTimeOfMessage().getTimeOfAnEvent().setValue(formatDatetime(messageDate));
+        msh.getMessageType().getMessageType().setValue(messageType);
+        msh.getMessageType().getTriggerEvent().setValue(triggerEvent);
+        msh.getMessageControlID().setValue(UUID.randomUUID().toString());
+        msh.getProcessingID().getProcessingID().setValue("P"); // P=Production, D=Debugging, T=Testing
+        msh.getVersionID().setValue("2.3"); // HL7 version targeted
+    }
+
+    /**
+     * @return the primary care identifier for the patient with any "-" character removed
+     */
+    public String getPatientIdentifier(Patient patient) {
+        PatientIdentifier patientIdentifier = patient.getPatientIdentifier(rwandaEmrConfig.getPrimaryCareIdentifierType());
+        return patientIdentifier == null ? null : patientIdentifier.getIdentifier().replace("-", "");
+    }
+
+    /**
+     * Patient Identifier should be trimmed
+     */
+    public void setPatientIdentifier(PID pid, Patient patient) throws HL7Exception {
+        pid.getSetIDPatientID().setValue("1");
+        String patientIdentifier = getPatientIdentifier(patient);
+        if (StringUtils.isBlank(patientIdentifier)) {
+            throw new IllegalStateException("Patient does not have a Primary Care ID");
+        }
+        pid.getPatientIDInternalID(0).getID().setValue(patientIdentifier);
+    }
+
+    /**
+     * Patient Name has a maximum length of 64 in the RIS.  Trim first and last to 30 to ensure it fits.
+     */
+    public void setPatientName(PID pid, Patient patient) throws HL7Exception {
+        pid.getPatientName(0).getFamilyName().setValue(trim(patient.getFamilyName(), 30));
+        pid.getPatientName(0).getGivenName().setValue(trim(patient.getGivenName(), 30));
+    }
+
+    public void setPatientBirthdate(PID pid, Patient patient) throws HL7Exception {
+        if (patient.getBirthdate() != null) {
+            pid.getDateOfBirth().getTimeOfAnEvent().setValue(formatDate(patient.getBirthdate()));
+        }
+    }
+
+    public void setPatientGender(PID pid, Patient patient) throws HL7Exception {
+        pid.getSex().setValue(patient.getGender() == null ? "O" : patient.getGender().toUpperCase());
+    }
+}
---
+++ api/src/test/java/org/openmrs/module/rwandaemr/radiology/PatientToADTTranslatorTest.java
@@ -0,0 +1,63 @@
+package org.openmrs.module.rwandaemr.radiology;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.blankOrNullString;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
+
+public class PatientToADTTranslatorTest extends BaseHL7TranslatorTest {
+
+    /**
+     * Example Message from Medsynapse RIS HL7 Integration Document
+     * MSH|^~\&|HIS_APP|HIS_FACILITY|PACS_APP|PACS_FACILITY|20130614131415||ADT^A08|MsgCtrlId_ADT|P|2.3
+     * EVN|A08|20130614131415
+     * PID|1||PatientID||PatientLast^First^Middle^^Title||20000514|F
+     */
+    @Test
+    public void shouldTranslateFromPatientToADT() throws Exception {
+        String dateTimeBefore = ymdhms.format(new Date());
+        String message = patientToADTTranslator.toADT_A08(patient);
+        String dateTimeAfter = ymdhms.format(new Date());
+
+        // The start of the MSH is expected to declare the separator character, so test that first
+        assertThat(message, startsWith("MSH|"));
+        // Add an extra delimiter so splitting works correctly
+        message = message.replace("MSH|", "MSH||");
+
+        testNumberOfFields(message, "MSH", 12);
+        testField(message, "MSH", 2, equalTo("^~\\&"));
+        testField(message, "MSH", 3, equalTo("OpenMRS"));  // sending application
+        testField(message, "MSH", 4, equalTo("HIS_FACILITY")); // sending facility
+        testField(message, "MSH", 5, equalTo("PACS_APP")); // receiving application
+        testField(message, "MSH", 6, equalTo("PACS_FACILITY")); // receiving facility
+        testField(message, "MSH", 7, greaterThanOrEqualTo(dateTimeBefore)); // Date/time of message
+        testField(message, "MSH", 7, lessThanOrEqualTo(dateTimeAfter)); // Date/time of message
+        testField(message, "MSH", 8, emptyString());
+        testField(message, "MSH", 9, equalTo("ADT^A08")); // Message Type
+        testField(message, "MSH", 10, not(blankOrNullString())); // Message Control ID
+        testField(message, "MSH", 11, equalTo("P"));
+        testField(message, "MSH", 12, equalTo("2.3"));
+
+        testNumberOfFields(message, "EVN", 2);
+        testField(message, "EVN", 1, equalTo("A08"));
+        testField(message, "EVN", 2, equalTo("20250414110000"));
+
+        testNumberOfFields(message, "PID", 8);
+        testField(message, "PID", 1, equalTo("1")); // Set ID
+        testField(message, "PID", 2, emptyString());
+        testField(message, "PID", 3, equalTo("P1234")); // Patient ID
+        testField(message, "PID", 4, emptyString());
+        testField(message, "PID", 5, equalTo("Hornblower^Horatio")); // Patient Name
+        testField(message, "PID", 6, emptyString());
+        testField(message, "PID", 7, equalTo(ymd.format(patient.getBirthdate())));  // Birthdate
+        testField(message, "PID", 8, equalTo("M"));  // Gender
+    }
+}
