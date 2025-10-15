0ede0233573e1bf2084414433a250381ef08369b
Michael Seaton <mseaton@pih.org>
2025-10-15 17:47:41 -0400

    RWA-1060 - Extract visit and login location configuration and selection to pihapps

---
+++ omod/src/main/java/org/openmrs/module/rwandaemr/page/controller/HomePageController.java
@@ -0,0 +1,53 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.rwandaemr.page.controller;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.appframework.context.AppContextModel;
+import org.openmrs.module.appframework.service.AppFrameworkService;
+import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.page.PageModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Spring MVC controller that takes over /index.htm and processes requests to show the home page so
+ * users don't see the legacy OpenMRS UI
+ */
+@Controller
+public class HomePageController {
+	
+	@RequestMapping("/index.htm")
+	public String overrideHomepage() {
+        return "forward:/rwandaemr/home.page";
+	}
+
+    /**
+     * @should limit which apps are shown on the homepage based on location
+     */
+    public Object controller(PageModel model,
+                             @SpringBean("appFrameworkService") AppFrameworkService appFrameworkService,
+                             UiSessionContext sessionContext) {
+
+        AppContextModel contextModel = sessionContext.generateAppContextModel();
+
+        model.addAttribute("extensions",
+                appFrameworkService.getExtensionsForCurrentUser("org.openmrs.referenceapplication.homepageLink", contextModel));
+        model.addAttribute("authenticatedUser", Context.getAuthenticatedUser());
+
+        return null;
+    }
+
+}
---
+++ omod/src/main/webapp/pages/home.gsp
@@ -0,0 +1,39 @@
+<%
+    ui.decorateWith("appui", "standardEmrPage", [ title: "Welcome to Rwanda EMR" ])
+    ui.includeCss("rwandaemr", "home.css")
+
+    def htmlSafeId = { extension ->
+        "${ extension.id.replace(".", "-") }-${ extension.id.replace(".", "-") }-extension"
+    }
+%>
+
+<script type="text/javascript">
+    jq(function() {
+        jq('#patient-search').focus();
+    });
+</script>
+
+<div id="home-container">
+
+    <% if (sessionContext.currentUser.hasPrivilege("App: coreapps.findPatient")) { %>
+
+    ${ ui.message("rwandaemr.searchPatientHeading") }
+    ${ ui.includeFragment("coreapps", "patientsearch/patientSearchWidget", [
+            showLastViewedPatients: 'false',
+            afterSelectedUrl: '/coreapps/clinicianfacing/patient.page?patientId={{patientId}}'
+    ])}
+    <% } %>
+
+
+    <div  class="col-12 col-sm-12 col-md-12 col-lg-12 homeList" id="apps">
+            <% extensions.each { ext -> %>
+                <a id="${ htmlSafeId(ext) }" href="/${ contextPath }/${ ext.url }" class="btn btn-default btn-lg button app big align-self-center" type="button">
+                    <% if (ext.icon) { %>
+                    <i class="${ ext.icon }"></i>
+                    <% } %>
+                    ${ ui.message(ext.label) }
+                </a>
+            <% } %>
+    </div>
+
+</div>
