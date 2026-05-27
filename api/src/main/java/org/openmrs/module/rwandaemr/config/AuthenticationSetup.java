44b1c681850bfbff6c669a732337ab3d94815432
Michael Seaton <mseaton@pih.org>
2026-05-27 12:56:56 -0400

    RWA-1082 - Upgrade rwandaemr to latest authentication modules to support email and remember functions

--- api/src/main/java/org/openmrs/module/rwandaemr/config/AuthenticationSetup.java
+++ api/src/main/java/org/openmrs/module/rwandaemr/config/AuthenticationSetup.java
@@ -24,6 +24,7 @@ public class AuthenticationSetup implements Setup {
 
     public static final String BASIC = "basic";
     public static final String SECRET = "secret";
+    public static final String EMAIL = "email";
     public static final String TOTP = "totp";
     public static final String TWO_FACTOR = "2fa";
 
@@ -33,26 +34,8 @@ public class AuthenticationSetup implements Setup {
         // Add this classloader
         AuthenticationConfig.registerClassLoader(AuthenticationSetup.class.getClassLoader());
 
-        // If no authentication scheme is explicitly configured, default to basic
+        // Configure 2fa
         AuthenticationConfig.setProperty(SCHEME, "2fa");
-
-        // We set up white list as everything needed for the basic login page and any additional scheme login page
-        // Add in any additional white list pages that are included in the config
-
-        Set<String> whitelist = new HashSet<>();
-        whitelist.add("/login.htm");
-        whitelist.add("/authenticationui/login/login.page");
-        whitelist.add("/appui/session/getLoginLocations.action");
-        whitelist.add("/csrfguard");
-        whitelist.add("/spa/**/*");
-        whitelist.add("*.js");
-        whitelist.add("*.css");
-        whitelist.add("*.gif");
-        whitelist.add("*.jpg");
-        whitelist.add("*.png");
-        whitelist.add("*.ico");
-        whitelist.add("*.ttf");
-        whitelist.add("*.woff");
 
         // Set up all the supported authentication schemes with default values.
         // Allow overriding with values from the config
@@ -64,7 +47,7 @@ public class AuthenticationSetup implements Setup {
             p.put("loginPage", "/authenticationui/login/login.page");
             p.put("usernameParam", "username");
             p.put("passwordParam", "password");
-            addScheme(BASIC, className, p, whitelist);
+            addScheme(BASIC, className, p);
         }
 
         // Secret Question Authentication Scheme.  This is an available 2nd factor
@@ -73,7 +56,16 @@ public class AuthenticationSetup implements Setup {
             Properties p = new Properties();
             p.put("loginPage", "/authenticationui/login/loginSecret.page");
             p.put("configurationPage", "/authenticationui/account/changeSecurityQuestion.page?schemeId={schemeId}&userId={userId}");
-            addScheme(SECRET, className, p, whitelist);
+            addScheme(SECRET, className, p);
+        }
+
+        // Email Authentication Scheme.  This is an available 2nd factor
+        {
+            String className = "org.openmrs.module.authentication.web.EmailAuthenticationScheme";
+            Properties p = new Properties();
+            p.put("loginPage", "/authenticationui/login/loginEmail.page");
+            p.put("configurationPage", "/authenticationui/account/configureEmail.page?schemeId={schemeId}&userId={userId}");
+            addScheme(EMAIL, className, p);
         }
 
         // Totp Authentication Scheme.  This is an available 2nd factor
@@ -83,7 +75,7 @@ public class AuthenticationSetup implements Setup {
             p.put("qrCodeIssuer", "RWANDA-EMR");
             p.put("loginPage", "/authenticationui/login/loginTotp.page");
             p.put("configurationPage", "/authenticationui/account/configureTotp.page?schemeId={schemeId}&userId={userId}");
-            addScheme(TOTP, className, p, whitelist);
+            addScheme(TOTP, className, p);
         }
 
         // Two-Factor Authentication Scheme.
@@ -91,11 +83,10 @@ public class AuthenticationSetup implements Setup {
             String className = "org.openmrs.module.authentication.web.TwoFactorAuthenticationScheme";
             Properties p = new Properties();
             p.put("primaryOptions", BASIC);
-            p.put("secondaryOptions", SECRET + "," + TOTP);
-            addScheme(TWO_FACTOR, className, p, whitelist);
+            p.put("secondaryOptions", SECRET + "," + TOTP + "," + EMAIL);
+            p.put("rememberMeEnabled", "true");
+            addScheme(TWO_FACTOR, className, p);
         }
-
-        AuthenticationConfig.setProperty(WHITE_LIST, String.join(",", whitelist));
 
         log.info("Authentication Schemes Configured");
         Properties p = AuthenticationConfig.getConfig();
@@ -110,7 +101,7 @@ public class AuthenticationSetup implements Setup {
     /**
      * Add configuration for a scheme with the given schemeId, if a scheme with this schemeId is not already configured
      */
-    protected static void addScheme(String schemeId, String className, Properties config, Set<String> whitelist) {
+    protected static void addScheme(String schemeId, String className, Properties config) {
         String schemeTypeProperty = SCHEME_TYPE_TEMPLATE.replace(SCHEME_ID, schemeId);
         if (StringUtils.isBlank(AuthenticationConfig.getProperty(schemeTypeProperty))) {
             AuthenticationConfig.setProperty(schemeTypeProperty, className);
@@ -119,9 +110,6 @@ public class AuthenticationSetup implements Setup {
                     String key = SCHEME_CONFIG_PREFIX_TEMPLATE.replace(SCHEME_ID, schemeId) + propertyName;
                     String value = config.getProperty(propertyName);
                     AuthenticationConfig.setProperty(key, value);
-                    if (propertyName.equalsIgnoreCase("loginPage")) {
-                        whitelist.add(value);
-                    }
                 }
             }
         }
