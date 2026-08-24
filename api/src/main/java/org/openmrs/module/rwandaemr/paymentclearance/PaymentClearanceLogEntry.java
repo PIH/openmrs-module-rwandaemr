8840ef65b8028ed6421a61e664e8317222bca037
nic10ben <nic10ben@gmail.com>
2026-08-24 16:37:26 +0200

    Add payment clearance SMS logging and service

---
+++ api/src/main/java/org/openmrs/module/rwandaemr/paymentclearance/PaymentClearanceLogEntry.java
@@ -0,0 +1,124 @@
+package org.openmrs.module.rwandaemr.paymentclearance;
+
+import java.util.Date;
+
+public class PaymentClearanceLogEntry {
+
+	private Integer globalBillId;
+	private Integer patientId;
+	private String patientIdentifier;
+	private String phoneNumber;
+	private String messageContent;
+	private String providerMessageId;
+	private String requestPayload;
+	private Integer responseCode;
+	private String responseBody;
+	private String status;
+	private String errorMessage;
+	private Integer creator;
+	private Date dateCreated;
+
+	public Integer getGlobalBillId() {
+		return globalBillId;
+	}
+
+	public void setGlobalBillId(Integer globalBillId) {
+		this.globalBillId = globalBillId;
+	}
+
+	public Integer getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(Integer patientId) {
+		this.patientId = patientId;
+	}
+
+	public String getPatientIdentifier() {
+		return patientIdentifier;
+	}
+
+	public void setPatientIdentifier(String patientIdentifier) {
+		this.patientIdentifier = patientIdentifier;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getMessageContent() {
+		return messageContent;
+	}
+
+	public void setMessageContent(String messageContent) {
+		this.messageContent = messageContent;
+	}
+
+	public String getProviderMessageId() {
+		return providerMessageId;
+	}
+
+	public void setProviderMessageId(String providerMessageId) {
+		this.providerMessageId = providerMessageId;
+	}
+
+	public String getRequestPayload() {
+		return requestPayload;
+	}
+
+	public void setRequestPayload(String requestPayload) {
+		this.requestPayload = requestPayload;
+	}
+
+	public Integer getResponseCode() {
+		return responseCode;
+	}
+
+	public void setResponseCode(Integer responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	public String getResponseBody() {
+		return responseBody;
+	}
+
+	public void setResponseBody(String responseBody) {
+		this.responseBody = responseBody;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public Integer getCreator() {
+		return creator;
+	}
+
+	public void setCreator(Integer creator) {
+		this.creator = creator;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+}
